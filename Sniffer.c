af574a49fae1b4c8e44ab3f66e2530e7bbc83739
nitayGitHub <nitaylevi1998@gmail.com>
2023-01-09 18:48:20 +0200

    firstCommit

---
+++ Sniffer.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <pcap.h>
+#include <stdint.h>
+
+typedef unsigned char u_char;
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+  printf("Got a packet\n");
+}
+
+// Packet Sniffing using the pcap API
+int main(int argc, char *argv[])
+{
+	char *dev = argv[1]; /* Device to sniff on */
+	char errbuf[PCAP_ERRBUF_SIZE];
+	struct bpf_program fp;
+	char filter_exp[] = "ip proto icmp"; /* The filter expression */
+	bpf_u_int32 net;					 /* The IP of our sniffing device */
+
+	printf("Device: %s\n", dev);
+
+	//  Step 1: Open live pcap session on NIC
+	pcap_t *handle; /* Session handle */
+	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (handle == NULL)
+	{
+		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		return (2);
+	}
+
+	// Step 2: Compile filter_exp into BPF psuedo-code
+	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1)
+	{
+		fprintf(stderr, "Couldn't compile filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return (2);
+	}
+
+	if (pcap_setfilter(handle, &fp) == -1)
+	{
+		fprintf(stderr, "Couldn't set filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return (2);
+	}
+
+	// Step 3: Capture packets
+	pcap_loop(handle, -1, got_packet, NULL);
+
+	pcap_close(handle); // Close the handle
+	return 0;
+}
